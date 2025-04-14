70b97cdb8a5bc0644622b2917bc24bbaaa1457f4
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2025-04-13 20:56:52 -0400

    feat: add refresh! method to MeliAuthToken using Net::HTTP
    + Implemented token refresh logic via POST request to MercadoLibre OAuth API
    + Used standard Net::HTTP, URI, and JSON libraries to avoid external dependencies
    + Parsed JSON response and updated access_token, refresh_token, and expires_in fields
    + Added error handling for HTTP responses

--- app/models/meli_auth_token.rb
+++ app/models/meli_auth_token.rb
@@ -1,6 +1,41 @@
+require "net/http"
+require "uri"
+require "json"
+require "ostruct"
+
 class MeliAuthToken < ApplicationRecord
   encrypts :access_token
   encrypts :refresh_token
 
   belongs_to :meli_account
+
+  def refresh!
+    uri = URI("https://api.mercadolibre.com/oauth/token")
+    request = Net::HTTP::Post.new(uri)
+    request["Content-Type"] = "application/x-www-form-urlencoded"
+    request["Accept"] = "application/json"
+
+    params = {
+      grant_type: "refresh_token",
+      client_id: Rails.application.credentials.dig(:meli, :app_id),
+      client_secret: Rails.application.credentials.dig(:meli, :client_secret),
+      refresh_token: self.refresh_token
+    }
+
+    request.body = URI.encode_www_form(params)
+
+    begin
+      response = http.request(request)
+      if response.is_a?(Net::HTTPSuccess)
+        data = JSON.parse(response.body)
+        update(
+          access_token: data["access_token"],
+          expires_in: data["expires_in"],
+          refresh_token: data["refresh_token"]
+          )
+      else
+          nil
+      end
+    end
+  end
 end
