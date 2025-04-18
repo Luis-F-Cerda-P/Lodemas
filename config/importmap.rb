4fe1c578867403ed9873c3311f731365e9e7c1da
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2025-04-18 18:56:16 -0400

    feat: integrate stimulus notification component
    + Added `@stimulus-components/notification` to importmap and registered it in Stimulus
    + Created vendor files for `@stimulus-components--notification.js` and `stimulus-use.js`
    + Updated `application.js` and `controllers/application.js` to support notification usage

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "flowbite", to: "flowbite.turbo.min.js"
+pin "@stimulus-components/notification", to: "@stimulus-components--notification.js" # @3.0.0
+pin "stimulus-use" # @0.52.3
