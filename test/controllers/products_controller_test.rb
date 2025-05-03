b48db4a5c585c648ebc54ca0deeb03c0f6e8abb9
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2025-05-03 19:59:42 -0400

    test: add full test coverage for ProductsController and Product model
    + Verify access control in ProductsController (ownership enforcement via OwnedResource)
    + Ensure correct rendering of only current user's products in views
    + Add fixture-backed controller tests for create, update, destroy, and unauthenticated access
    + Add model validations, attachments, and rich text tests for Product
    + Include test image fixture for ActiveStorage tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,4 +4,118 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @user = users(:one)
+    @other_user = users(:two)
+    @product = products(:shoes)
+    @product.update(user: @user)
+
+    # Log in using Rails 8 authentication
+    post session_url, params: {
+      email_address: @user.email_address,
+      password: "L123456" # Assuming this is the fixture password
+    }
+  end
+
+  test "should get index showing only current user's products" do
+    other_product = products(:pants)
+    # other_product.update(user: @other_user)
+
+    get products_url
+    assert_response :success
+
+    # Should include user's product
+    assert_select "#product_#{@product.id}"
+    # Should not include other user's product
+    assert_select "#product_#{other_product.id}", 0
+  end
+
+  test "should show user's product" do
+    get product_url(@product)
+    assert_response :success
+  end
+
+  test "should not show other user's product" do
+    other_product = products(:pants)
+    other_product.update(user: @other_user)
+
+    get product_url(other_product)
+    assert_response :not_found # Assuming you're raising ActiveRecord::RecordNotFound
+  end
+
+  test "should get new" do
+    get new_product_url
+    assert_response :success
+  end
+
+  test "should create product associated with current user" do
+    assert_difference("@user.products.count") do
+      post products_url, params: {
+        product: {
+          name: "New Product",
+          inventory_count: 5
+        }
+      }
+    end
+
+    product = Product.last
+    assert_equal @user.id, product.user_id
+    assert_redirected_to product_url(product)
+  end
+
+  test "should get edit for user's product" do
+    get edit_product_url(@product)
+    assert_response :success
+  end
+
+  test "should update user's product" do
+    patch product_url(@product), params: {
+      product: {
+        name: "Updated Product Name"
+      }
+    }
+
+    @product.reload
+    assert_equal "Updated Product Name", @product.name
+    assert_redirected_to product_url(@product)
+  end
+
+  test "should destroy user's product" do
+    assert_difference("Product.count", -1) do
+      delete product_url(@product)
+    end
+
+    assert_redirected_to products_url
+  end
+
+  test "should not be able to manipulate other user's products" do
+    other_product = products(:pants)
+    other_product.update(user: @other_user)
+
+    # Try to edit
+    get edit_product_url(other_product)
+    assert_response :not_found
+
+    # Try to update
+    patch product_url(other_product), params: { product: { name: "Hacked Product" } }
+    assert_response :not_found
+    other_product.reload
+    assert_not_equal "Hacked Product", other_product.name
+
+    # Try to destroy
+    assert_no_difference("Product.count") do
+      delete product_url(other_product)
+    end
+    assert_response :not_found
+  end
+
+  test "should redirect to login when not authenticated" do
+    # Logout first
+    delete session_url
+
+    # Try accessing products
+    get products_url
+    assert_redirected_to new_session_path
+  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,6 +2,16 @@ require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
   include ActionMailer::TestHelper
+  include ActionDispatch::TestProcess
+
+  setup do
+    @user = users(:one)
+    @valid_attributes = {
+      name: "Test Product",
+      inventory_count: 10,
+      user: @user
+    }
+  end
 
   test "sends email notification when back in stock" do
     product = products(:shoes)
@@ -11,5 +21,55 @@ class ProductTest < ActiveSupport::TestCase
     assert_emails 2 do
       product.update(inventory_count: 99)
     end
+  end
+
+  test "should be valid with valid attributes" do
+    product = Product.new(@valid_attributes)
+    assert product.valid?
+  end
+
+  test "should require a name" do
+    product = Product.new(@valid_attributes.merge(name: nil))
+    assert_not product.valid?
+    assert_includes product.errors[:name], "can't be blank"
+  end
+
+  test "should require inventory_count to be non-negative" do
+    product = Product.new(@valid_attributes.merge(inventory_count: -1))
+    assert_not product.valid?
+    assert_includes product.errors[:inventory_count], "must be greater than or equal to 0"
+  end
+
+  test "should allow inventory_count of zero" do
+    product = Product.new(@valid_attributes.merge(inventory_count: 0))
+    assert product.valid?
+  end
+
+  test "should belong to a user" do
+    product = Product.new(@valid_attributes.merge(user: nil))
+    assert_not product.valid?
+    assert_includes product.errors[:user], "must exist"
+  end
+
+  test "should have rich text description" do
+    product = Product.create(@valid_attributes)
+    product.description = ActionText::Content.new("Product description")
+    product.save
+    assert_equal "Product description", product.description.to_plain_text
+  end
+
+  test "should be able to attach featured image" do
+    product = Product.create(@valid_attributes)
+    file = fixture_file_upload("test_image.jpg", "image/jpeg")
+
+    assert_difference -> { ActiveStorage::Attachment.count } do
+      product.featured_image.attach(file)
+    end
+
+    assert product.featured_image.attached?
+  end
+
+  test "should include Notifications module" do
+    assert_includes Product.included_modules, Product::Notifications
   end
 end
