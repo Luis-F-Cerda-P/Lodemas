c074541eb6760b52795b83920b1905f031ceb386
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2025-05-03 23:04:06 -0400

    test: fix meli_notifications_controller test to use POST instead of GET
    + Updated the test to use POST request instead of incorrect GET
    + Provided a valid sample payload for the notification
    + Ensured request is sent as JSON with appropriate headers

--- test/controllers/api/meli_notifications_controller_test.rb
+++ test/controllers/api/meli_notifications_controller_test.rb
@@ -1,8 +1,17 @@
 require "test_helper"
 
 class Api::MeliNotificationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get api_meli_notifications_create_url
+  test "should respond with success to valid POST" do
+    post api_meli_notifications_url, params: {
+      resource: "/orders/2195160686",
+      user_id: 468424240,
+      topic: "orders_v2",
+      application_id: 5503910054141466,
+      attempts: 1,
+      sent: "2019-10-30T16:19:20.129Z",
+      received: "2019-10-30T16:19:20.106Z"
+    }, as: :json
+
     assert_response :success
   end
 end
